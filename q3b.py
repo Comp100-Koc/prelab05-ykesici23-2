275366542e3263ae18c9c2d8f774608e427cb7aa
ykesici23 <ykesici23@ku.edu.tr>
2026-04-03 05:39:12 +0300

    Update q3b.py

--- q3b.py
+++ q3b.py
@@ -2,4 +2,28 @@ def add_binary(a, b):
     '''
     Given two strings perform binary addition and return the result as a string
     '''
-    pass
+    if a.startswith(''):
+        a = a[2:]
+    if b.startswith(''):
+        b = b[2:]
+    a, b = fill_zeros(a, b)
+    carry = 0
+    sonuc = ''
+    for i in range(len(a)-1, -1, -1):
+        bit_sum = (int(a[i])) + (int(b[i])) + carry
+        sonuc = str(bit_sum % 2) + sonuc
+        carry = bit_sum // 2
+    if carry:
+        sonuc = '1' + sonuc
+    i = 0
+    while i < len(sonuc) and sonuc[i] == '0':
+        i += 1
+    sonuc = sonuc[i:]
+    if not sonuc:
+        sonuc = '0'
+    return '0b' + sonuc
+def fill_zeros(a, b):
+    maxlen = max(len(a), len(b))
+    a = ('0' * (maxlen - len(a))) + a
+    b = ('0' * (maxlen - len(b))) + b
+    return a, b    
