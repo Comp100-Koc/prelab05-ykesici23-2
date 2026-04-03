37e8283ecee752403dec2a474745f533f11992c6
ykesici23 <ykesici23@ku.edu.tr>
2026-04-03 05:35:12 +0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -2,4 +2,21 @@ def remove_adjacent_duplicates(s):
     '''
     Given a string remove all the adjacent duplicate characters and return the string
     '''
-    pass
+    while adjacent_duplicates_kontrol(s):
+        sonuc = ""
+        for i in range(len(s)):
+
+            if i < len(s)-1 and s[i] == s[i+1]:
+  
+                sonuc += s[i+2:]
+                break
+            else:
+                sonuc += s[i]
+
+        s = sonuc
+    return s
+def adjacent_duplicates_kontrol(s):
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            return True
+    return False
